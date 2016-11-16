d15e15e7f523d062a47ac8828d7fea5acc2ca210
Michal Hoftich <michal.h21@gmail.com>
2016-11-16 12:58:54 +0100

    Simplified root node usage

--- dom-sample.lua
+++ dom-sample.lua
@@ -1,23 +1,23 @@
 --kpse.set_program_name("luatex")
-function traverseDom(parser, current,level)
-	local level = level or 0
-        local spaces = string.rep(" ",level)
-	local root= current or parser._handler.root
-	local name = root._name or "unnamed"
-	local xtype = root._type or "untyped"
-	local attributes = root._attr  or {} 
-	if xtype == "TEXT" then 
-		print(spaces .."TEXT : " .. root._text)
-	else	 
-		print(spaces .. xtype .. " : " .. name) 
-	end
-	for k, v in pairs(attributes) do
-		print(spaces .. "  ".. k.."="..v)
-	end
-	local children = root._children or {}
-	for _, child in ipairs(children) do
-		traverseDom(parser,child, level + 1)
-	end
+function traverseDom(current,level)
+  local level = level or 0
+  local spaces = string.rep(" ",level)
+  local root= current or current.root
+  local name = root._name or "unnamed"
+  local xtype = root._type or "untyped"
+  local attributes = root._attr  or {} 
+  if xtype == "TEXT" then 
+    print(spaces .."TEXT : " .. root._text)
+  else	 
+    print(spaces .. xtype .. " : " .. name) 
+  end
+  for k, v in pairs(attributes) do
+    print(spaces .. "  ".. k.."="..v)
+  end
+  local children = root._children or {}
+  for _, child in ipairs(children) do
+    traverseDom(child, level + 1)
+  end
 end
 
 local xml = require('luaxml-mod-xml')
@@ -26,4 +26,4 @@ local x = '<p>hello <a href="http://world.com/">world</a>, how are you?</p>'
 local domHandler = handler.domHandler()
 local parser = xml.xmlParser(domHandler)
 parser:parse(x)
-traverseDom(parser)
+traverseDom(domHandler.root)
