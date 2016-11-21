e042057534f6b9046edabf36e52fd3421a770663
Michal Hoftich <michal.h21@gmail.com>
2016-11-21 15:35:03 +0100

    Print internal structure

--- ldoc-latex.lua
+++ ldoc-latex.lua
@@ -4,6 +4,19 @@ return {
          for _, item in ipairs(mod.items) do
             if item.type == 'function' and not item.ret then
                print(mod.name,item.name,mod.file,item.lineno)
+               local par = {}
+               for k,v in pairs(item.params or {}) do
+                 print("parameter",k,v)
+               end
+               for k, v in pairs(item) do
+                 print ("item", k,v)
+               end
+               print("return",item.ret)
+               print "----------"
+             else
+               print "***********"
+               print(mod, item.type)
+               print "***********"
             end
          end
       end
