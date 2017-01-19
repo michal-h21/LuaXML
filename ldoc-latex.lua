e2b7e9fb68d251bbb16954407f1de7299fcf71d7
Michal Hoftich <michal.h21@gmail.com>
2017-01-19 17:15:49 +0100

    Print more properties

--- ldoc-latex.lua
+++ ldoc-latex.lua
@@ -2,14 +2,18 @@ return {
    filter = function (t)
       for _, mod in ipairs(t) do
          for _, item in ipairs(mod.items) do
-            if item.type == 'function' and not item.ret then
+            if item.type == 'function' then
                print(mod.name,item.name,mod.file,item.lineno)
                local par = {}
-               for k,v in pairs(item.params or {}) do
-                 print("parameter",k,v)
+               local map = item.params.map or {}
+               for k,v in ipairs(item.params or {}) do
+                 print("parameter",k,v, map[v], map[k])
                end
                for k, v in pairs(item) do
                  print ("item", k,v)
+               end
+               for k, v in pairs(item.modifiers or {}) do
+                 print("modifier", k,v)
                end
                print("return",item.ret)
                print "----------"
