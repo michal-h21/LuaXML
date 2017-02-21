554578010f2cc5fbc6544f3ebec113cf76d5135c
Michal Hoftich <michal.h21@gmail.com>
2017-02-21 17:28:48 +0100

    Test selecto matching

--- test/cssquery-test.lua
+++ test/cssquery-test.lua
@@ -35,13 +35,32 @@ describe("CSS selector handling", function()
     assert.same(#matchedlist, 3)
   end)
   describe("List selectors that matches object", function()
+    -- this should match two elements with "ahoj" class
     obj:add_selector(".ahoj", function(domobj)
+      domobj:set_attribute("style", "color:green")
       return false
     end)
+    -- but the one with "pokus" id should block to use the class match
     obj:add_selector("#pokus", function(domobj)
+      domobj:set_attribute("style", "color:red")
       return false
     end)
-    for k,v in ipairs(obj.selectors) do
+    -- Rule for #pokus should be first in the selectors list
+    it("Automatic specificity sorting should work", function()
+      assert.same(obj.querylist[1].specificity, 100)
+    end)
+    local span_ahoj = newobj:query_selector "span.ahoj" [1]
+    local div_ahoj  = newobj:query_selector "div.ahoj" [1]
+    it("query_selector should work", function()
+      assert.same(span_ahoj:get_element_name(), "span")
+      assert.same(div_ahoj:get_element_name(), "div")
+    end)
+    it("Saved query matches should work", function()
+      assert.same(#obj:match_querylist(span_ahoj), 1)
+      -- should match .ahoj and #pokus 
+      assert.same(#obj:match_querylist(div_ahoj), 2)
+    end)
+    for k,v in ipairs(obj.querylist) do
       print(k, v.source, v.specificity)
     end
   end)
