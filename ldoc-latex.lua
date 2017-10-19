12efbbe2dbba0b0dd0286c9186db6f662ab1a45b
Michal Hoftich <michal.h21@gmail.com>
2017-10-19 17:31:34 +0200

    code cleanup

--- ldoc-latex.lua
+++ ldoc-latex.lua
@@ -1,8 +1,9 @@
 local function escape(s)
   local escapes = {
-    _ = "\\_{}"
+    _ = "\\_{}",
+    ["\\"] = "\\backspace{}"
   }
-  return s:gsub("([_])", function(a) return escapes[a] end)
+  return s:gsub("([_\\])", function(a) return escapes[a] end)
 end
 
 local function print_template(format, s)
@@ -38,57 +39,24 @@ return {
   filter = function (t)
     local modules = {}
     for modid, mod in ipairs(t) do
+      -- print basic information about module
       print_module(mod)
       local classes = {}
       for _, item in ipairs(mod.items) do
         if item.type == 'function' then
+          -- move functions to tables corresponding to their classes
           local curr_class = item.section
           if curr_class then
             local class = classes[curr_class] or {}
             class[#class+1] = item
             classes[curr_class] = class
-          else
-            -- print "Outside class"
           end
-          -- print(mod.name,item.name,mod.file,item.lineno)
-          local par = {}
-          local map = item.params.map or {}
-          for k,v in ipairs(item.params or {}) do
-            -- print("parameter",k,v, map[v], map[k])
-          end
-          for k, v in pairs(item) do
-            -- print ("item", k,v)
-          end
-          for k, v in pairs(item.modifiers or {}) do
-            -- print("modifier", k,v)
-            for _, yyy in pairs(v) do
-              for x,y in pairs(yyy) do
-                -- print("mod", x,y)
-              end
-            end
-          end
-          -- print("return",item.ret)
-          -- print "----------"
-        else
-          -- print "***********"
-          -- print(mod, item.type)
-          -- print "***********"
         end
       end
       for k,v in pairs(classes) do
+        -- print class info and functions
         print_class(mod, k,v)
-        -- print("class", k, #v)
-        -- if type(v) == "table" then
-        -- for x,y in pairs(v) do
-        -- print("", x,y)
-        -- end
-        -- end
       end
-      -- for _,section in ipairs(mod.sections) do
-      --   for k,v in pairs(section) do
-      --     print("mod", k,v)
-      --   end
-      -- end
     end
   end
 }
