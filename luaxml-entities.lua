5ecd5403d32a47897bb8c44892d3d1e773fa69d8
Michal Hoftich <michal.h21@gmail.com>
2018-02-09 14:11:13 +0100

    Fixed decimal entity decoding

---
+++ luaxml-entities.lua
@@ -0,0 +1,37 @@
+local M = {}
+local char = unicode.utf8.char
+local named_entities = require "luaxml-namedentities"
+local hexchartable = {}
+local decchartable = {}
+
+
+local function get_named_entity(name)
+  return named_entities[name]
+end
+
+function M.decode(s)
+  return s:gsub("&([#a-zA-Z0-9]+);?", function(m)
+    -- check if this is named entity first
+    local named = get_named_entity(m)
+    if named then return named end
+    -- check if it is numeric entity
+    local hex, charcode = m:match("#([xX]?)([a-fA-F0-9]+)")
+    -- if the entity is not numeric
+    if not charcode then return 
+      "&" .. m .. ";" 
+    end
+    local character 
+    if hex~="" then
+      character = hexchartable[charcode] or char(tonumber(charcode,16))
+      hexchartable[charcode] = character
+    else
+      character = decchartable[charcode] or char(tonumber(charcode))
+      decchartable[charcode] = character
+    end
+    return character
+  end)
+end
+
+return M
+
+
