2f3ca566bbbd99780924aa9489264d7ba4ecfb40
Michal Hoftich <michal.h21@gmail.com>
2018-02-09 13:29:44 +0100

    Save named entities

---
+++ data/jsontolua.lua
@@ -0,0 +1,12 @@
+-- convert json file with html named entities to Lua table
+-- json source: https://html.spec.whatwg.org/entities.json
+local json = require "dkjson"
+local data = io.read("*all")
+
+local json_data = json.decode(data)
+print("return {")
+for name, rec in pairs(json_data) do
+  print(string.format('%s="%s",',name:gsub("[&;]", ""), rec.characters))
+end
+
+print "}"
