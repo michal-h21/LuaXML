1649684c4cbd580a59d6993d3c5e3bff38850c21
Michal Hoftich <michal.h21@gmail.com>
2021-07-02 15:47:52 +0200

    Test tranform object

--- test/transform-test.lua
+++ test/transform-test.lua
@@ -5,11 +5,18 @@ local dom = require "luaxml-domobject"
 local transform = require "luaxml-transform"
 
 describe("Basic DOM functions", function() 
+  local transformer1 = transform.new()
+  local transformer2 = transform.new()
   local text="<b>hello</b>"
-  transform.add_action("b", "transform1: %s")
+  transformer1:add_action("b", "transform1: %s")
+  transformer2:add_action("b", "transform2: %s")
   it("should do basic transformations", function()
-    assert.same("transform1: hello", transform.parse_xml(text))
+    assert.same("transform1: hello", transformer1:parse_xml(text))
   end)
+  it("should support multiple tranformer objects", function()
+    assert.same("transform2: hello", transformer2:parse_xml(text))
+  end)
+
 
   
 end)
