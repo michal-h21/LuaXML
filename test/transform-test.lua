99124fa884a5d1c78bb086c1a83930efc8ce1df3
Michal Hoftich <michal.h21@gmail.com>
2021-07-07 15:48:21 +0200

    tests for selectors

--- test/transform-test.lua
+++ test/transform-test.lua
@@ -1,7 +1,7 @@
 require "busted.runner" ()
 kpse.set_program_name "luatex"
 
-local dom = require "luaxml-domobject"
+local domobject = require "luaxml-domobject"
 local transform = require "luaxml-transform"
 
 describe("Basic DOM functions", function() 
@@ -16,7 +16,36 @@ describe("Basic DOM functions", function()
   it("should support multiple tranformer objects", function()
     assert.same("transform2: hello", transformer2:parse_xml(text))
   end)
-
-
-  
 end)
+
+describe("Transform DOM object", function()
+  local transformer = transform.new()
+  local dom = domobject.parse  [[<section>hello <b>world</b></section>]]
+  transformer:add_action("section", "sect: %s")
+  transformer:add_action("b", "b: %s")
+  it("should transform dom object", function()
+    assert.same("sect: hello b: world", transformer:process_dom(dom))
+  end)
+end)
+
+describe("selectors support", function()
+  local transformer = transform.new()
+  local dom1 = domobject.parse  [[<x>hello <b>world</b></x>]]
+  local dom2 = domobject.parse  [[<v>hello <b>world</b></v>]]
+  local dom3 = domobject.parse  [[<v>hello <b class="hello">world</b></v>]]
+  local dom4 = domobject.parse  [[<v>hello <b id="id">world</b></v>]]
+
+
+  transformer:add_action("x b", "xb: %s")
+  transformer:add_action("v b", "vb: %s")
+  transformer:add_action(".hello", "hello: %s")
+  transformer:add_action("#id", "id: %s")
+  it("should support css selectors", function()
+    assert.same("hello xb: world", transformer:process_dom(dom1))
+    assert.same("hello vb: world", transformer:process_dom(dom2))
+    assert.same("hello hello: world", transformer:process_dom(dom3))
+    assert.same("hello id: world", transformer:process_dom(dom4))
+  end)
+
+end)
+
