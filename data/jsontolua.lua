7f8e51454c58c304503b7249edefa517e7bec494
Michal Hoftich <michal.h21@gmail.com>
2025-02-21 13:25:34 +0100

    sort namedentites alphabetically

--- data/jsontolua.lua
+++ data/jsontolua.lua
@@ -3,9 +3,20 @@
 local json = require "dkjson"
 local data = io.read("*all")
 
+local function sorted(json_data)
+  -- we need to sort entity names alphabetically to get good order each time we run this script (for git)
+  local t = {}
+  for k in pairs(json_data) do
+    table.insert(t, k)
+  end
+  table.sort(t)
+  return t
+end
+
 local json_data = json.decode(data)
 print("return {")
-for name, rec in pairs(json_data) do
+for _, name in ipairs(sorted(json_data)) do
+  local rec = json_data[name]
   print(string.format('["%s"]="%s",',name:gsub("[&;]", ""), rec.characters:gsub('\\', '\\\\'):gsub("\n", '\\n'):gsub('"', '\\"')))
 end
 
